60e0d6e536522445fd3a61614e723098cdd1861e
Justin Johnson <triplejay2013@gmail.com>
2018-03-02 14:13:47 -0700

    Trying a new solution...scrapping everything

--- 18/main.cpp
+++ 18/main.cpp
@@ -8,48 +8,99 @@ using namespace std;
 const string EXAMPLE="./example.txt";
 const string NUM_PYRAMID="./numPyramid.txt";
 vector<vector<int>> pyramid;
-int rows = -1;//offset to make it work
+vector<int> total;
+int rows;
+//cols == rows
 
-int maxRouteSum(){
-	//first value at top of pyramid
-	int current = pyramid[0][0], sum = 0, max = 0;
-	for(int i = 1; i < rows; ++i){//skip the first one
-		for(int j = 0; j < i+1; ++j){
-			if(pyramid[i][j]){
+int sum(){
+	int sum = 0;
+	for(int i = 0; i < total.size(); ++i){
+		sum += total[i];
+	}
+	return sum;
+}
 
-			}
+//0003
+//0074
+//0246
+//8593
+void maxRouteSum(){
+	//save the first row value...top of the pyramid
+	total.push_back(pyramid[0][rows-1]);
+	int j = rows-1;
+	//skip first row
+	for(int i = 1; i < rows; ++i){
+		if(pyramid[i][j-1] > pyramid[i][j]){
+			total.push_back(pyramid[i][j-1]);
+			//move cols left one
+			j-=1;
+		}
+		else{
+			//straight down
+			total.push_back(pyramid[i][j]);
 		}
 	}
+	for(int i = 0; i < total.size(); ++i){
+		cout << total[i] << ", ";
+	}
+	cout << endl;
 }
 
 void print(){
 	for(int i = 0; i < rows; ++i){
-		for(int j = 0; j < i+1; ++j){
-			cout << pyramid[i][j];
+		for(int j = 0; j < rows; ++j){
+			if(to_string(pyramid[i][j]).length()==1){
+				cout << "    " << pyramid[i][j];
+			}
+			else if(to_string(pyramid[i][j]).length()==2){
+				cout << "   " << pyramid[i][j];
+			}
+			else{
+				cout << "  " << pyramid[i][j];
+			}
 		}
 		cout << endl;
 	}
+}
+
+int getRows(string file){
+	string temp;
+	int sum = 0;
+	ifstream ff(file);
+	while(getline(ff,temp)){
+		++sum;
+	}
+	ff.close();
+	return sum;
 }
 
 void load(string file) {
 	cout << "Loading " << file << endl;
 	ifstream ff(file);
 	int temp;
-
-	for(int counter = 1; !ff.eof(); ++counter){
-		vector<int> tempVec;
-		for(int j = 1; j < counter+1; ++j){
-			ff >> temp;
-			if(ff.eof()) break;
-			tempVec.push_back(temp);
-			rows = counter;//the final value of counter (the rows)
+	rows = getRows(file);
+	for(int i = 0; i < rows; ++i){
+		vector<int> vec;
+		//fills in row with necessary amount of zeros
+		for(int j = rows-1; j > i; --j){
+			vec.push_back(0);
 		}
-		pyramid.push_back(tempVec);
+		//creates the actual triangle
+		for(int j = 0; j < i + 1; ++j){
+			ff >> temp;
+			vec.push_back(temp);
+		}
+		pyramid.push_back(vec);
 	}
+
+	ff.close();
 }
 
 int main(){
-	load(EXAMPLE);
+	//load(EXAMPLE);
+	load(NUM_PYRAMID);
 	print();
+	maxRouteSum();
+	cout << sum() << endl;
 	return 0;
 }
