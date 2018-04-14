984233f3c275f487255a21da3a171730e8a3498d
Justin Johnson <triplejay2013@gmail.com>
2018-04-14 16:19:08 -0600

    Finished problem 33. produced the four fractions in  a sloppy way and did some mental math

--- 33/digitCancel.py
+++ 33/digitCancel.py
@@ -28,19 +28,22 @@ def reduceFraction(num, den):
 
 # num(erator), den(ominator)
 def digitCancel(num, den):
-  num = list(str(num))
-  den = list(str(den))
-  ret = ""
+  n = list(str(num))
+  d = list(str(den))
 
-  for i in num:
-    for j in den:
+  done = False
+  for i in n:
+    for j in d:
       # if the digits are the same
       if i==j:
-        num.remove(i)
-        den.remove(j)
+        n.remove(i)
+        d.remove(i)
+        done = True
         break
-  ret = num[0] + "/" + den[0]
-  return ret
+    if done: break
+  if done:
+    return n[0] + "/" + d[0]
+  return ""
 
 while True:
   start = clock()
@@ -54,10 +57,11 @@ while True:
 
   for i in range(10,100):
     for j in range(i+1, 100):
-      print(reduceFraction(num,den))
-      cancel = digitCancel(num,den)
-      # check simplist form of digitcancel
-      if reduceFraction(int(cancel[0]), int(cancel[2])) == reduceFraction(num,den):
-        print(cancel)
+      if i % 10 == 0 or j % 10 == 0:
+        continue
+      cancel = digitCancel(i,j)
+      if len(cancel) == 0: continue
+      if reduceFraction(int(cancel[0]), int(cancel[2])) == reduceFraction(i,j):
+        print(str(i)+"/"+ str(j) +" => " + cancel)
   end = clock()
   print("The program took %f seconds to run" % (end-start))
