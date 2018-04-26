e4255755efeedaf8fdc419758959108dcf6cff49
Justin Johnson <triplejay2013@gmail.com>
2018-04-26 16:07:35 -0600

    updating earlier projects to be faster and better (and in python)

--- 1/multiples.py
+++ 1/multiples.py
@@ -1,20 +1,29 @@
-# finds all multiples of a given number and finds their sum
-# 
-# Parameters: 
-#	factor1 is the first factor (ex:3)
-#	factor2 is the second factor (ex:5)
-#	limit is the limit of numbers we will consider (ex:10)
-#
-# Return: 
-#	sum of all factors below the limit
-def multipleSum(factor1, factor2, limit):
-    sum = 0
-    for i in range(1,limit):
-        if i%factor1 == 0 or i%factor2 == 0:
-            sum+=i
-    return sum
-            
+from time import *
 
-#print and call multipleSum()
-print("multipleSum(3,5,10)", multipleSum(3,5,10))
-print("multipleSum(3,5,1000)", multipleSum(3,5,1000))
+def multipleSum(factor1, factor2, limit):
+  total = 0
+  for i in range(1,limit):
+    if i%factor1== 0 or i%factor2 == 0:
+      total+=i
+  return total
+
+print("""
+    
+Multiples of 3 and 5
+Problem 1
+
+  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23.
+
+  Find the sum of all the multiples of 3 or 5 below 1000.
+
+  Answer: 233168
+
+  """)
+while True:
+  factor1 = int(input("Enter first Factor: "))
+  factor2 = int(input("Enter second Factor: "))
+  limit = int(input("Enter limit: "))
+  start=clock()
+  print("The multiple sum of {} and {} for numbers under {} is {}".format(factor1, factor2, limit,multipleSum(factor1,factor2,limit)))
+  end = clock()
+  print("Your program took {} seconds to run".format(end-start))
